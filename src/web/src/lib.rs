f10cdcedb0fbc44cbc37dafc3ba5f1f581b3bed3
rahff <raphaelandrey99@gmail.com>
2025-01-28 19:30:10 +0100

    fix tests in core due to type improvment

--- src/web/src/lib.rs
+++ src/web/src/lib.rs
@@ -6,17 +6,14 @@ pub mod app;
 mod event_queue;
 mod route_handler;
 
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
 
 #[cfg(test)]
 mod tests {
-    use super::*;
+    use crate::app::app;
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    #[tokio::test]
+    async fn it_works() {
+        tokio::spawn(app());
+
     }
 }
